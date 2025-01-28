7c1d216eca9c3742591bae04a4e435a1d0a08749
Jason Blackburn <blackburnjason1994@gmail.com>
2025-01-28 14:52:22 +0000

    about section added

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -1,64 +1,23 @@
-"use client";
 import "./about.css";
 
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-import { useRef, useEffect } from "react";
-
-gsap.registerPlugin(ScrollTrigger);
+import Link from "next/link";
 
 export default function About() {
-  const aboutRef = useRef(null);
-
-  const tl = gsap.timeline();
-
-  useGSAP(() => {
-    tl.to(".about", {
-      margin: "20px",
-      boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-      scrollTrigger: {
-        trigger: ".about",
-        start: "top 50%",
-        end: "center 50%",
-        toggleActions: "play reverse play reverse",
-        scrub: true,
-      },
-    })
-      .to(".about-header", {
-        opacity: 1,
-        x: 0,
-        scrollTrigger: {
-          trigger: ".about",
-          start: "-20% 50%",
-          end: "20% 50%",
-          toggleActions: "play reverse play reverse",
-          scrub: true,
-        },
-      })
-      .to(".about-content", {
-        y: 0,
-        scrollTrigger: {
-          trigger: ".about",
-          start: "-20% 50%",
-          end: "20% 50%",
-          toggleActions: "play reverse play reverse",
-          scrub: true,
-        },
-      });
-  });
-
   return (
-    <section className="about" ref={aboutRef}>
-      <h2 className="about-header">About</h2>
-      <div className="about-content-wrapper">
-        <p className="about-content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsum
-          odio quibusdam ullam. Blanditiis, velit, dolor vel aliquid quasi
-          tempora, sapiente aliquam hic quaerat optio magnam inventore ducimus!
-          Magni, inventore?
+    <section className="about-section">
+      <div className="about-wrapper">
+        <h2 className="about-header">Who are Enhance?</h2>
+        <p className="about-p">
+          We are an experienced Development Technical Consultancy that provide a
+          multi product service for all aspects of build and planning. If you
+          require a simple extension application all the way through to full
+          scale housing/commercial development we are your one stop shop
+          advisory Technical expert.
         </p>
+        <div className="about-link-wrapper">
+          <Link href={"/"}>Contact</Link>
+          <Link href={"/"}>Services</Link>
+        </div>
       </div>
     </section>
   );
--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -12,11 +12,13 @@ export default function Hero() {
   return (
     <section className="hero" ref={heroRef}>
       <div className="hero-content">
-        <div className="hero-container-left">
-          <h1 className="hero-heading">Enhance</h1>
-          <h2 className="hero-subheading">Technical Consulting</h2>
+        <div className="hero-container">
+          <div>
+            <h1 className="hero-heading">Enhance</h1>
+            <h2 className="hero-subheading">Technical Consulting</h2>
+          </div>
+          <p className="tagline">Enhance, Support, Thrive</p>
         </div>
-        <div className="hero-container-right"></div>
       </div>
       <video autoPlay muted loop playsInline className="hero-video">
         <source src="/hero.mp4" type="video/mp4" />
