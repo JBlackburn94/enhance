5c3347996d88b5280b36a5d143ca5786c186e700
Jason Blackburn <blackburnjason1994@gmail.com>
2025-02-28 10:47:04 +0000

    footer added

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,34 @@
+import Link from "next/link";
 import "./footer.css";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div></div>
+        <div></div>
+      </div>
+      <div className="footer-links">
+        <hr />
+        <div className="footer-lower">
+          <div className="footer-info">
+            <p>© 2025 Enhance Technical Consulting. All rights reserved.</p>
+            <p>Website Design & Development by Jason Blackburn</p>
+          </div>
+          <div className="social-icons">
+            <Link href={"#"}>
+              <FaFacebook size={48} />
+            </Link>
+            <Link href={"#"}>
+              <FaInstagram size={48} />
+            </Link>
+            <Link href={"#"}>
+              <FaLinkedin size={48} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
       <About />
       <Portfolio />
       <Banner />
-      <Footer />
     </main>
   );
 }
