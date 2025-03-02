21f5ee26fb841e09acc9d0f62e1ae0b1b4964e39
Jason Blackburn <blackburnjason1994@gmail.com>
2025-03-02 13:37:19 +0000

    unusued footer import removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
 import Portfolio from "./components/companyInfo/Company";
-import Footer from "./components/footer/Footer";
 import Banner from "./components/banner/Banner";
 
 export default function Home() {
