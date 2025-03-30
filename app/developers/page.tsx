b77c341888de0efa0c6e8b01b5aecd972acfe8b8
Jason Blackburn <blackburnjason1994@gmail.com>
2025-03-30 14:43:08 +0100

    adds homeowners content

--- app/developers/page.tsx
+++ app/developers/page.tsx
@@ -1,4 +1,4 @@
-import ServicePage from "../components/ServicePage/ServicePage";
+import Image from "next/image";
 import developerImage from "../../public/developer-card-image.jpg";
 
 import "./developers.css";
@@ -6,21 +6,65 @@ import "./developers.css";
 export default function page() {
   return (
     <section className="services">
-      <ServicePage
-        image={developerImage}
-        title="Developers"
-        content="As the building industry continues to expand and competition grows
-            fiercer, it is becoming ever more critical to utilise resources that
-            are available to you. That's where we come in."
-        extraContent="At Enhance we empower developers to foster collaborative working to
-            further drive progression within their business. We offer a wide
-            range of support that is varied to each individual client at any
-            stage of the build process."
-        listDesc="The 3 key stages of developer services we provide are:"
-        listItems={[
-          "Land acquisition/bid appraisal work which incorporates high end due diligence and technical reporting, inclusive of constraints identification & initial engineering appraisals to consider all costs.",
-        ]}
-      />
+      <div>
+        <div className="image-container">
+          <Image
+            src={developerImage}
+            alt="An image of a housing development"
+            className="services-image"
+          />
+        </div>
+        <div className="content">
+          <h1>Developer</h1>
+          <div className="content-paragraph">
+            <p>
+              As the building industry continues to expand and competition grows
+              fiercer, it is becoming ever more critical to utilise resources
+              that are available to you, in order to win a site and commence
+              building.
+            </p>
+          </div>
+          <hr />
+          <div className="content-paragraph">
+            <p></p>
+            <ul>
+              <li className="font-bold">Pre-Development</li>
+              <div className="inner-list-item">
+                <li>
+                  Land acquisition/bid appraisal work which incorporates high
+                  end due diligence and technical reporting, inclusive of
+                  constraints identification & initial engineering appraisals to
+                  consider all costs.
+                </li>
+                <li>
+                  Cost analysis tendering & instruction for recommended reports
+                  ahead of planning
+                </li>
+                <li>Planning submission support and management.</li>
+              </div>
+              <li className="main-list-item">Live Development</li>
+              <div className="inner-list-item">
+                <li>Project lead on demolition, refurbs and new build.</li>
+                <li>
+                  Project management throughout any of the build stage,
+                  inclusive of utility coordination.
+                </li>
+                <li>Planning & building control condition discharge.</li>
+              </div>
+              <li className="main-list-item">Post-Development</li>
+              <div className="inner-list-item">
+                <li>Adoption and site closure services.</li>
+              </div>
+            </ul>
+            <div className="content-paragraph">
+              <p>
+                Should you have any queries or wish to enquire further regarding
+                the services we can offer, please contact us.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- app/homeowners/page.tsx
+++ app/homeowners/page.tsx
@@ -1,9 +1,67 @@
-import React from "react";
+import Image from "next/image";
+import homeownerImage from "../../public/homeowner-card-image.jpg";
+
+import "./homeowners.css";
 
 export default function page() {
   return (
-    <section className="h-screen flex justify-center items-center">
-      homeowners
+    <section className="services">
+      <div>
+        <div className="image-container">
+          <Image
+            src={homeownerImage}
+            alt="An image of a housing development"
+            className="services-image"
+          />
+        </div>
+        <div className="content">
+          <h1>Homeowner</h1>
+          <div className="content-paragraph">
+            <p>
+              Considering an extension to add more space or simply to increase
+              the properties worth, submitting a planning application can be
+              somewhat daunting if you are unfamiliar with what is required.
+            </p>
+            <p>
+              At Enhance we can provide expert advice and support as much as its
+              required to ensure a smooth planning application.
+            </p>
+          </div>
+          <hr />
+          <div className="content-paragraph">
+            <h2>What we offer:</h2>
+            <ul>
+              <div className="inner-list-item">
+                <li>Advice on permitted development rights.</li>
+                <li>Submit planning applications for consideration.</li>
+                <li>
+                  Submit and coordinate building control application and
+                  documentation.
+                </li>
+                <li>
+                  Produce initial basic drawing designs & proposals for
+                  submission.
+                </li>
+                <li>
+                  Provide recommendations on local trustworthy Architects and
+                  Engineers.
+                </li>
+                <li>
+                  Provide reccomendations on local trustworthy contractors.
+                </li>
+              </div>
+            </ul>
+            <div className="content-paragraph">
+              <p>
+                The items above allow you the homeowner to have as much
+                involvement or as little as you like. Advice, support and
+                project management can be provided to suit your needs and your
+                budget.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
